beda83ee0bf84a18f418fdb0167023f39546b84c
Prasham-Parekh <prasham.parekh@gmail.com>
2025-01-19 11:43:39 -0500

    added navbar and css features

---
+++ index.js
@@ -0,0 +1,11 @@
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+  item.addEventListener('click', function() {
+    navItems.forEach(i => i.classList.remove('clicked'));
+    navItems.forEach(i => i.classList.add('not-clicked'));
+    
+    this.classList.remove('not-clicked');
+    this.classList.add('clicked');
+  });
+});
